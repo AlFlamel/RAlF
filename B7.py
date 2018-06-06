b33ae36cdb70dfd979050c15a6e85f72ea475934
AlFlamel <39924179+AlFlamel@users.noreply.github.com>
2018-06-06 15:45:28 +0300

    Update B7.py

--- B7.py
+++ B7.py
@@ -1,51 +1,31 @@
 import datetime
 
-################################################################
-def printTimeStamp(name):									  ##
- print('Автор програми: ' + name)							  ##
- print('Час компіляції: ' + str(datetime.datetime.now()))	  ##
-print(printTimeStamp('Beliaev and Pereviznyk'))				  ##
-################################################################
+def printTimeStamp(name):									  
+ print('Автор програми: ' + name)							  
+ print('Час компіляції: ' + str(datetime.datetime.now()))	  
+
 print("Місце проживання (Гуртожиток, Квартира, Будинок)")
-home = input('Введіть місце проживання: ')
+home = input("Введіть місце проживання: ")
 
-time = int(input('Введіть скільки годин ви приблизно знаходитесь дома: '))
-print('                                           ')
+time = int(input("Введіть скільки годин ви приблизно знаходитесь дома: "))
 
-if home =='Гуртожиток':
-	#Мурашник 
-	if time < 6:
-	  print('Тобі підійде: Мурашник')
-	#Мурашник або Рибки
-    elif time == 6 :
-      print('Тобі підійде: Мурашник або рибки')
-    #Рибки 
-    elif time > 6:
-	  print('Тобі підійдуть: Рибки')
-
-elif  home =='Квартира':
-	#Хом'як
-	if time <10 :
-      print("Тобі підійде: Хом'як")
-
-
-
-#Хом'як або Кішка
-elif home =='Квартира' and time == 10 :
+if(home == "Гуртожиток") and (time < 6):
+	print("Тобі підійде: Мурашник")
+elif(home == "Гуртожиток") and (time == 6):
+  print("Тобі підійде: Мурашник або рибки")
+elif(home == "Гуртожиток") and (time > 6):
+  print("Тобі підійдуть: Рибки")
+elif(home == "Квартира") and (time < 10):
+  print("Тобі підійде: Хом'як")
+elif(home =="Квартира") and (time == 10):
 	print("Тобі підійде: Хом'як або Кішка")
-
-#Кішка
-elif home =='Квартира' and time > 10:
+elif(home =='Квартира') and (time > 10):
 	print('Тобі підійде: Кішка')
-
-#Змія
-elif home =='Будинок' and time <=10 :
+elif(home =='Будинок') and (time <= 10):
 	print("Тобі підійде: Змія")	
-
-#Собака
-elif home =='Будинок' and time >10 and time <17:
+elif(home =='Будинок') and (time > 10) and (time < 17):
 	print("Тобі підійде: Собака")
+elif (home =='Будинок') and (time > 18):
+	print("Тобі підійде: В’єтнамське порося")
 
-#В’єтнамське порося
-elif home =='Будинок' and time > 18:
-	print('Тобі підійде: В’єтнамське порося')
+printTimeStamp("Alexey.")
