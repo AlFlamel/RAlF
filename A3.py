6d1f7b977158961e573f1689c61eab381b79748c
AlFlamel <39924179+AlFlamel@users.noreply.github.com>
2018-06-06 15:06:37 +0300

    Update A3.py

--- A3.py
+++ A3.py
@@ -1,12 +1,12 @@
 import datetime
 
+def printTimeStamp(name):
+ print('Автор програми: ' + name)
+ print('Час компіляції: ' + str(datetime.datetime.now()))
+
 print("число в кПа: ")
 a = float(input())
 
 print ("фт/дм2: ", a*0.145038)
-name = "Serga i Yan"
-def printTimeStamp(name):
- print('Автор програми: ' + name)
- print('Час компіляції: ' + str(datetime.datetime.now()))
  
-printTimeStamp(name)
+printTimeStamp("Alexey.")
