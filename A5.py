9c21f77eb74a8865df523ca05d0f963e4b185a9e
AlFlamel <39924179+AlFlamel@users.noreply.github.com>
2018-06-06 15:14:34 +0300

    Update A5.py

--- A5.py
+++ A5.py
@@ -1,2 +1,10 @@
+import datetime 
+
+def printTimeStamp(name): 
+  print('Автор програми: ' + name) 
+  print('Час компіляції: ' + str(datetime.datetime.now()))
+
 name = str(input("Как вас зовут?.."))
 print("Здравствуйте " + name)
+
+printTimeStamp("Alexey.")
