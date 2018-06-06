d3379c4353f04ad221de566c3e3ebab8ffa4c40c
AlFlamel <39924179+AlFlamel@users.noreply.github.com>
2018-06-06 15:27:00 +0300

    Update A27.py

--- A27.py
+++ A27.py
@@ -1,7 +1,15 @@
+import datetime 
+
+def printTimeStamp(name): 
+  print('Автор програми: ' + name) 
+  print('Час компіляції: ' + str(datetime.datetime.now()))
+
 i1 = float(input("Яка зараз година? \n"))
 i2 = input("Чи говорить попугай? \n")
 if  i2 == "Так" and (i1 <= 8 or i1 == 22 or i1 == 23 or i1 == 24):
 	print ("Накрити ковдрою")
 else:
 	print("Не накривати ковдрою")
+
+printTimeStamp("Alexey.")
 
